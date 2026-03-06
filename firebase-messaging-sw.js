99ac679fa59c1b97bedd7a62342b47aa964e7475
thanwyaa <fbwk4974@gmail.com>
2026-03-06 23:04:37 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,11 +1,27 @@
+// استدعاء مكتبات Firebase للإشعارات في الخلفية
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSy...", // مش لازم تحط بياناتك هنا عادي
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Z8iHqNNfWEWrwIfP15x3hlh65GEZ0mQ",
+  authDomain: "sanaayee.firebaseapp.com",
   projectId: "sanaayee",
-  messagingSenderId: "777421869818",
-  appId: "1:777421869818:web:..."
-});
+  storageBucket: "sanaayee.firebasestorage.app",
+  messagingSenderId: "1048456368002",
+  appId: "1:1048456368002:web:e37c8e2b7ebba18ef2499b"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+
+// معالجة الإشعار لما يوصل والموقع مقفول
+messaging.onBackgroundMessage((payload) => {
+  console.log('وصل إشعار في الخلفية: ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://thanwyaa.github.io/saanaye/logo.png', // حط رابط لوجو موقعك هنا
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
